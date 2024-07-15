174fabf1b3fc3a4fdcfccc9511ea09a8346af4e6
saisubhasish <saisubhasishrout777@gmail.com>
2024-07-15 16:49:56 +0530

    Updated streamlit code

--- app.py
+++ app.py
@@ -1,65 +1,39 @@
-import json
-import boto3
-import botocore.config
-from datetime import datetime
+import requests
+import streamlit as st
 
+from src.logger import logging
+from src.exception import BlogException
 
-def generate_blog_using_bedrock(blogtopic:str)->str:
-    prompt=f"""<s>[INST]Human: Write a 200 words blog post about {blogtopic}.
-    Assistant:[/INST]
-    """
-
-    body={
-        "prompt":prompt,
-        "max_gen_len":512,
-        "temperature":0.5,
-        "top_p":0.9
-    }
-
+def get_bedrock_response(blog_topic):
     try:
-        bedrock=boto3.client("bedrock-runtime", region_name="us-east-1",
-                             config=botocore.config.Config(
-                                read_timeout=300,
-                                retries={'max_attempts': 3}))
-        response = bedrock.invoke_model(body=json.dumps(body),modelId="meta.llama3-8b-instruct-v1:0")
+        logging.info("'main': Post request to the backend API with given data")
+        response = requests.post(
+            "https://f5z0m20hu5.execute-api.us-east-1.amazonaws.com/dev/blog_generation",
+            json={
+                'blog_topic': blog_topic
+            }
+        )
+        logging.info(f"'main': Request response: {response}")
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        return {'error': str(e)}
 
-        response_content = response.get('body').read()
-        response_data = json.loads(response_content)
-        print(response_data)
-        blog_details = response_data['generation']
-        return blog_details
-    
-    except Exception as e:
-        print(f"Error in generating the blog: {e}")
-        return ""
+st.title('Blog Generator')
 
-def save_blog_details_s3(s3_key, s3_bucket, generated_blog):
-    s3=boto3.client('s3')
+with st.form("my_form"):
+    st.write("Fill the input fields below: ")
+    blog_topic = st.text_input('Enter the topic name')
+    logging.info(f"'main': Topic name: {blog_topic}")
 
-    try:
-        s3.put_object(Bucket=s3_bucket, Key=s3_key, Body=generated_blog)
-        print('Code saved to s3')
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        logging.info("'main': Data submitted, now invoking the Backend API")
+        response = get_bedrock_response(blog_topic)
+        logging.info(f"'main': Generated response: {response}")
+        if 'error' in response:
+            st.error(f"Error: {response['error']}")
+        else:
+            st.write(response)
 
-    except Exception as e:
-        print(f"Error in saving the blog: {e}")
-
-def lambda_handler(event, context):
-    # TODO implement
-    event = json.loads(event['body'])
-    blogtopic = event['blog_topic']
-
-    generate_blog = generate_blog_using_bedrock(blogtopic=blogtopic)
-
-    if generate_blog:
-        current_time = datetime.now().strftime('%H%M%S')
-        s3_key = f"blog-output/{current_time}.txt"
-        s3_bucket='awsbedrockbloggeneration'
-        save_blog_details_s3(s3_key, s3_bucket, generate_blog)
-
-    else:
-        print('No blog was generated')
-
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Blog generated successfully!')
-    }
+st.write("Thank you for using the blog generator.")
